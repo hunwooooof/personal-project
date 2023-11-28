8b97bb5aafe7c35d727627f1a6ad630631b5e3af
Wanyu Lee <wanyulee1998@gmail.com>
2023-11-28 17:00:19 +0800

    [style] Adjust students page ui

--- sol-basketball-system/src/pages/admin/Students/index.tsx
+++ sol-basketball-system/src/pages/admin/Students/index.tsx
@@ -25,42 +25,44 @@ function Student() {
   }, []);
 
   return (
-    <div className='custom-main-container mt-28'>
-      <div className='w-10/12 mx-auto'>
-        <div className='mt-6  px-3 py-2 text-xl border-b border-gray-200 mb-8'>Students</div>
-        <div className='flex flex-wrap gap-5 pb-8'>
-          {students &&
-            students.map((student) => {
-              const calculate_age = (birthday: string) => {
-                const dateOfBirth = new Date(birthday);
-                const diff = Date.now() - dateOfBirth.getTime();
-                const age = new Date(diff);
-                return Math.abs(age.getUTCFullYear() - 1970);
-              };
-              return (
-                <div
-                  className='flex flex-col shrink-0 items-center w-56 h-80 border shadow-md p-4 rounded-md'
-                  key={student.id}>
-                  <img src={student.photoURL} className='w-24 h-24 object-cover rounded-full mb-5' />
-                  <div className='text-xl text-black mb-2'>
-                    {student.firstName} {student.lastName}
+    <div className='custom-main-container pt-16'>
+      <div className='w-11/12 mx-auto'>
+        <div className='custom-page-title mb-6'>Students</div>
+        <div className='w-full h-[77vh] bg-white rounded-3xl p-6'>
+          <div className='flex flex-wrap gap-4 justify-center h-[71vh] overflow-y-auto rounded-md'>
+            {students &&
+              students.map((student) => {
+                const calculate_age = (birthday: string) => {
+                  const dateOfBirth = new Date(birthday);
+                  const diff = Date.now() - dateOfBirth.getTime();
+                  const age = new Date(diff);
+                  return Math.abs(age.getUTCFullYear() - 1970);
+                };
+                return (
+                  <div
+                    className='flex flex-col items-center w-56 h-88 p-5 rounded-xl bg-slate-100 font-bold text-gray-500'
+                    key={student.id}>
+                    <img src={student.photoURL} className='w-24 h-24 object-cover rounded-full mb-5' />
+                    <div className='text-xl text-black mb-2'>
+                      {student.firstName} {student.lastName}
+                    </div>
+                    <div className=' text-black mb-5'>{student.chineseName}</div>
+                    <div className='flex w-8/12 gap-1 mb-2 items-center'>
+                      {Cake()}
+                      {student.birthday}
+                    </div>
+                    <div className='flex w-8/12 gap-1 mb-2 items-center'>
+                      {School()}
+                      {student.school}
+                    </div>
+                    <div className='flex w-8/12 gap-1 items-center'>
+                      {Age()}
+                      {calculate_age(student.birthday)} years old
+                    </div>
                   </div>
-                  <div className=' text-black mb-5'>{student.chineseName}</div>
-                  <div className='flex w-8/12 gap-1 mb-2 items-center'>
-                    {Cake()}
-                    {student.birthday}
-                  </div>
-                  <div className='flex w-8/12 gap-1 mb-2 items-center'>
-                    {School()}
-                    {student.school}
-                  </div>
-                  <div className='flex w-8/12 gap-1 items-center'>
-                    {Age()}
-                    {calculate_age(student.birthday)} y
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
+          </div>
         </div>
       </div>
     </div>
