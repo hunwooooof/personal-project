0435861576e6b51bc11b380e858c1bf165db6a3b
Wanyu Lee <wanyulee1998@gmail.com>
2023-11-28 19:26:33 +0800

    [style] Adjust sign up and log in page ui

--- sol-basketball-system/src/pages/Signup/index.tsx
+++ sol-basketball-system/src/pages/Signup/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import googleLogoUrl from '../../assets/google-logo.png';
 import logoUrl from '../../assets/sol-logo.png';
 import { useStore } from '../../store/store';
 
@@ -28,19 +29,18 @@ function Signup() {
   }, [isLogin]);
 
   return (
-    <div className='custom-main-container'>
-      <div className='mt-36 mx-auto w-96 flex items-center flex-col'>
-        <div>
-          <img src={logoUrl} alt='sign-up-icon' className='w-24' />
-          <div className=' text-center'>Sign up</div>
-        </div>
-        <div className='flex flex-col gap-2 w-full'>
+    <div className='custom-main-container pt-14'>
+      <div className='mt-10 mx-auto w-5/12 flex items-center flex-col'>
+        <img src={logoUrl} alt='sign-up-icon' className='w-20 mb-5' />
+        <div className='flex flex-col gap-5 w-full bg-white p-8 rounded-3xl mb-8'>
+          <div className='text-2xl text-gray-500 text-center'>Sign up</div>
           <input
             type='text'
             name='name'
             id='name'
             placeholder='Name *'
             className='custom-signin-input'
+            value={account.name}
             onChange={handleInputChange}
           />
           <input
@@ -49,6 +49,7 @@ function Signup() {
             id='email'
             placeholder='Email Address *'
             className='custom-signin-input'
+            value={account.email}
             onChange={handleInputChange}
           />
           <input
@@ -57,20 +58,27 @@ function Signup() {
             id='password'
             placeholder='Password *'
             className='custom-signin-input'
+            value={account.password}
             onChange={handleInputChange}
-          />
-          <button
-            type='submit'
-            className='text-white bg-[#1876D1] text-center shadow-md px-4 py-2 rounded-sm'
-            onClick={() => nativeSignup(account)}>
-            SIGN UP
-          </button>
+          />{' '}
+          <div className='flex flex-col'>
+            <button
+              type='submit'
+              className='text-white bg-slate-500 text-center p-2 rounded-3xl hover:bg-slate-800'
+              onClick={() => nativeSignup(account)}>
+              SIGN UP
+            </button>
+            <Link to='/login' className='text-sm self-end underline mt-2 text-gray-400 hover:text-blue-500'>
+              Already have an account? Sign in
+            </Link>
+          </div>
         </div>
-        <Link to='/login' className='self-end text-blue-500 underline'>
-          Already have an account? Sign in
-        </Link>
-        <div>or</div>
-        <button onClick={googleLogin}>使用 Google 帳戶註冊</button>
+        <button
+          onClick={googleLogin}
+          className='flex items-center gap-3 bg-white rounded-full px-5 py-3 text-gray-400 hover:text-black'>
+          <img src={googleLogoUrl} alt='google logo' className='w-6' />
+          使用 Google 帳戶註冊
+        </button>
       </div>
     </div>
   );
